125293012425e9fdae0aab3ba7a97f6717a1e857
Yochanan Scharf <yochanan@protrack.co.il>
2024-10-13 15:40:23 +0300

    scikit

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,22 @@ from streamlit import session_state as ss
 from PIL import Image
 from transformers import AutoFeatureExtractor, AutoModel
 import joblib
-
+from time import sleep
 st.title('X-Ray classifier')
+sidebar = st.sidebar
 
 
+
+def predict_image(image):
+    inputs = ss.feature_extractor(images=image_arr, return_tensors="pt")
+    with torch.no_grad():
+        outputs = ss.model_embeddings(**inputs)
+    embeddings = outputs.last_hidden_state[:, 0].squeeze().numpy()
+    label = ss.model_svm.predict(embeddings.reshape(1, -1))
+    return label
+
+
+    
 if 'count' not in ss:
 
     ss.count = 0
@@ -17,26 +29,25 @@ if 'count' not in ss:
     model = AutoModel.from_pretrained(model_name)
     ss.model_embeddings = model
     ss.model_svm = joblib.load('svm_model_final.joblib')
-
-if __name__ == '__main__':
-    
-
-    
-    img_file = None
-    
-    img_file = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"], key = ss.count)
+    st.experimental_rerun()
+    #img_file = None
+else:    
+    img_file = sidebar.file_uploader("Upload an image (sometimes you need to upload twice!)", type=["png", "jpg", "jpeg"], key = ss.count)
     if img_file is not None:
         
           
 
         image_arr = Image.open(img_file).convert("RGB")
         st.image(image_arr)
-        inputs = ss.feature_extractor(images=image_arr, return_tensors="pt")
-        with torch.no_grad():
-            outputs = ss.model_embeddings(**inputs)
-        embeddings = outputs.last_hidden_state[:, 0].squeeze().numpy()
-        label = ss.model_svm.predict(embeddings.reshape(1, -1))
-        st.write('The label is {}'.format(label+1))
+        label = predict_image(image_arr)
+        # inputs = ss.feature_extractor(images=image_arr, return_tensors="pt")
+        # with torch.no_grad():
+        #     outputs = ss.model_embeddings(**inputs)
+        # embeddings = outputs.last_hidden_state[:, 0].squeeze().numpy()
+        # label = ss.model_svm.predict(embeddings.reshape(1, -1))
+        sidebar.write('The label is {}'.format(label+1), )
         img_file = None
 
         ss.count += 1
+        sleep(5)
+        st.experimental_rerun()
